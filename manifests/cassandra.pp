15debfe8394b4c84293f38c3dbec87e43af8c314
yuxcer <yuxcer@126.com>
2012-11-08 15:45:07 +0800

    add required package

--- manifests/cassandra.pp
+++ manifests/cassandra.pp
@@ -1,7 +1,12 @@
 class kanyun::cassandra {
+	exec { 'pip install pycassa':
+			path => '/usr/bin',
+			require => Package['python-pip'],
+		}
     exec {'python /etc/puppet/modules/cassandra/files/init_cassandra_db.py':
-    	  path => '/usr/bin',
+      path => '/usr/bin',
 	  logoutput => true ,
 	  require => Class['cassandra'],
+	  subscribe  => Exec['pip install pycassa'],
   }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -24,25 +24,13 @@ class kanyun(
 ){
   include 'concat::setup'
 
-  apt::source { "sws_v2":
-    location          => "http://10.67.21.10/swsv2",
-    release           => "",
-    repos             => "binary/",
-    pin               => "1001",
-    include_src       => false
-  }
-
-  exec { 'apt-get update':
-    path                => '/usr/bin',
-    refreshonly         => true,
-    subscribe           => [ Apt::Source["sws_v2"]],
-    logoutput           => true,
-  }
-
   package { 'kanyun-common':
 	ensure	=> present,
+	require => Package['sws-common']
   }
- 
+  package { 'sws-common':
+	ensure  => present,
+ }
   file { '/etc/kanyun':
     ensure  => directory,
     owner   => 'kanyun',
