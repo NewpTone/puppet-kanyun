28b07cc56313beae532ad37cbbbb587f4e996d0a
yuxcer <yuxcer@126.com>
2012-11-08 15:47:22 +0800

    add python-pip package

--- manifests/cassandra.pp
+++ manifests/cassandra.pp
@@ -1,8 +1,11 @@
 class kanyun::cassandra {
+	package {'python-pip':
+			ensure => present,
+	}
 	exec { 'pip install pycassa':
 			path => '/usr/bin',
 			require => Package['python-pip'],
-		}
+	}
     exec {'python /etc/puppet/modules/cassandra/files/init_cassandra_db.py':
       path => '/usr/bin',
 	  logoutput => true ,
